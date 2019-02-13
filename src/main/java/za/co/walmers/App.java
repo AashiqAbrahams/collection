845909cf55a1d75993cf6c4c98bd482bbf804e45
Aashiq Abrahams <aashiqabrahamszb@gmail.com>
2019-02-13 18:23:54 +0200

    testing2

--- src/main/java/za/co/walmers/App.java
+++ src/main/java/za/co/walmers/App.java
@@ -10,12 +10,6 @@ public class App
     {
 
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
-        System.out.println( "Goodbye World..." );
-=======
-
-        System.out.println( "Hello World!, you have been edited" );
->>>>>>> 76a827b2c6757d265822877e0602fd02b803e086
 
     }
 }
